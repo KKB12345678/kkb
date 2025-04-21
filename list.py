2a70fb5be425c699fc2146660772859d27eebd76
KKB12345678 <kathirbalaji99@gmail.com>
2025-04-21 09:50:41 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+list=["a",12.32,45,7+9j,"python1",34.2,True]
+print(list)
+print(type(list))
+print(list[0])
+print(list[1])
+print(list[0:2])
+print(list[-1])
+print(list*3)
+print(list + [12,34.25,"python2","ka",23])
